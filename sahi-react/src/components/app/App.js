443a2996610f5804f0b5d3ebc7baf16b924a49a9
MaxiIvan <85696496+maxiivandlf@users.noreply.github.com>
2022-02-28 17:07:31 -0300

    fin del formulario

--- sahi-react/src/components/app/App.js
+++ sahi-react/src/components/app/App.js
@@ -1,10 +1,12 @@
 import '../app/App.css';
-import Login from '../login/Login.js';
+// import Login from '../register/login/Login.js';
+// import Signup from '../register/signup/signup';
+import Appp from '../../Prueba_compts/funComp/App2';
 
 function App() {
   return (
     <div className='App'>
-      <Login />
+      <Appp />
     </div>
   );
 }
